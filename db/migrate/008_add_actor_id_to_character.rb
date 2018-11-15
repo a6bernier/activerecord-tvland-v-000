e712f11798a31032be70cd2102893462ff5a4dd6
Andrew Bernier <a6bernier@gmail.com>
2018-11-15 23:18:13 +0000

    automatically backed up by learn

--- db/migrate/008_add_actor_id_to_character.rb
+++ db/migrate/008_add_actor_id_to_character.rb
@@ -1,6 +1,6 @@
-class AddDayAndSeasonToShow < ActiveRecord::Migration[4.2]
-   def change
-    add_column :shows, :day, :string
-    add_column :shows, :season, :string
+
+class AddActorIdToCharacters < ActiveRecord::Migration[4.2]
+  def change
+    add_column :characters, :actor_id, :integer
   end
 end
